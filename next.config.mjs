3eeec2da0843cea4127ddceedc4f164f79ce5e5a
Lewis-Lian <79491428@qq.com>
2025-09-17 12:20:41 +0800

    build: 添加 Next.js 配置
    - 在 next.config.mjs 中添加了新的配置项
    - 设置 output 为 'export'，用于静态站点生成
    - 注释中提供了 trailingSlash 和 skipTrailingSlashRedirect 的使用示例

--- next.config.mjs
+++ next.config.mjs
@@ -8,3 +8,13 @@ const config = {
 };
 
 export default withMDX(config);
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+};
