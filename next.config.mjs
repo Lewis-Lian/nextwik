b8cdb048137f4315b82ca32bff94fd1dd4a9e2da
Lewis-Lian <79491428@qq.com>
2025-09-17 12:35:10 +0800

    ci: 更新 GitHub Actions 工作流并简化 Next.js 配置
    - 在 GitHub Actions 工作流中启用 configure-pages 步骤
    - 移除 Next.js 配置中的冗余设置

--- next.config.mjs
+++ next.config.mjs
@@ -17,18 +17,4 @@ const nextConfig = {
   // trailingSlash: true,
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
-  basePath: process.env.BASE_PATH,
-  assetPrefix: process.env.ASSET_PREFIX,
-  reactStrictMode: true,
-  output: isProd ? 'export' : 'standalone',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  }
 };
-//next.config.js
-const isProd = process.env.NODE_ENV === 'production'
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
-})
