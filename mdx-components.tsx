a26be09a11a9617c4368650cb1560ceed0e8fd3d
Lewis-Lian <79491428@qq.com>
2025-09-19 15:24:22 +0800

    docs(sudynotes): 更新 Git 安装教程
    - 调整文档结构，优化标题层级
    - 添加图标装饰，提升视觉效果
    - 详细解释每个安装步骤，增强可读性
    - 引入 Callout 组件，突出重要信息
    - 修正部分翻译错误，提高准确性
    - 更新 MDX 组件，支持更多格式

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,24 +1,33 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
-import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
+import * as FilesComponents from 'fumadocs-ui/components/files';
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import type { MDXComponents } from 'mdx/types';
-import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
-import { compile } from '@mdx-js/mdx';
-import { rehypeCode } from 'fumadocs-core/mdx-plugins';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import * as icons from 'lucide-react';
 
-// use this function to get MDX components, you will need it for rendering MDX
-export function getMDXComponents(components?: MDXComponents): MDXComponents {
+export function getMDXComponents(components?: MDXComponents) {
   return {
+    ...(icons as unknown as MDXComponents),
     ...defaultMdxComponents,
-    img: (props) => <ImageZoom {...(props as any)} />,
-        // HTML `ref` attribute conflicts with `forwardRef`
-    pre: ({ ref: _ref, ...props }) => (
-      <CodeBlock {...props}>
-        <Pre>{props.children}</Pre>
-      </CodeBlock>
-    ),
+    ...TabsComponents,
+    ...FilesComponents,
+    Accordion,
+    Accordions,
     ...components,
-  };
+  } satisfies MDXComponents;
 }
-await compile('...', {
-  rehypePlugins: [rehypeCode],
-});
+
+declare module 'mdx/types.js' {
+  // Augment the MDX types to make it understand React.
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    type Element = React.JSX.Element;
+    type ElementClass = React.JSX.ElementClass;
+    type ElementType = React.JSX.ElementType;
+    type IntrinsicElements = React.JSX.IntrinsicElements;
+  }
+}
+
+declare global {
+  type MDXProvidedComponents = ReturnType<typeof getMDXComponents>;
+}
