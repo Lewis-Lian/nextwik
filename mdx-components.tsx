b92056dd9fcbf1fbb47d342b16d65a01c0c663ce
Lewis-Lian <79491428@qq.com>
2025-09-19 10:10:03 +0800

    docs(sudynotes): 删除冗余章节并更新索引页面
    - 删除了多个冗余章节文件，包括第1章、第2章等
    - 更新了 sudynotes 索引页面，添加了 Fumadocs 简介内容
    - 在 mdx-components.tsx 中添加了 ImageZoom 和 CodeBlock 组件支持

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,24 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import type { MDXComponents } from 'mdx/types';
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
+import { compile } from '@mdx-js/mdx';
+import { rehypeCode } from 'fumadocs-core/mdx-plugins';
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: (props) => <ImageZoom {...(props as any)} />,
+        // HTML `ref` attribute conflicts with `forwardRef`
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     ...components,
   };
 }
+await compile('...', {
+  rehypePlugins: [rehypeCode],
+});
